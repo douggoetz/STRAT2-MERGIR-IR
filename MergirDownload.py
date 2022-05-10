ec7012b9e3a9c217ae7b5bb92a69bf51c83e6f5c
douggoetz <55999091+douggoetz@users.noreply.github.com>
2022-05-10 14:10:36 -0400

    Update MergirDownload.py
    comment out unused libraries

--- MergirDownload.py
+++ MergirDownload.py
@@ -4,11 +4,11 @@ from pydap.cas.urs import setup_session
 import pandas as pd
 import numpy as np
 import datetime
-import pprint
+#import pprint
 from math import radians, cos, sin, asin, sqrt
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-import imageio
+#import imageio
 import os
 
 ## Credentials for NASA EarthData Account
